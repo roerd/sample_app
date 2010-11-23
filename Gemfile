c6379b99546a073b2d600095487c7fe471953d70
Roerd Hinrichsen <roerdhh@gmail.com>
2010-11-23 12:10:39 +0100

    Initial revision.

---
+++ Gemfile
@@ -0,0 +1,24 @@
+# -*- mode: ruby -*-
+
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.3'
+
+# Bundle edge Rails instead:
+# gem 'rails', :git => 'git://github.com/rails/rails.git'
+
+gem 'sqlite3-ruby', :require => 'sqlite3'
+
+# Use mongrel as the web server
+gem 'mongrel', '1.2.0.pre2'
+
+# Use RSpec for testing
+
+group :development do
+  gem 'rspec-rails', '2.0.1'
+end
+
+group :test do
+  gem 'rspec', '2.0.1'
+  gem 'webrat', '0.7.1'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '2351eb6c6091a79beda03da23ccd3af2486551779192cff95b091b3945da4e08c8461abafb23e2963420b6f7a881351ae5be7a80300785cb736e7febc339f421'
