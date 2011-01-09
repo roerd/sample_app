94a601178fa5fab7ef578350e59a5bae0d7c5e89
Roerd Hinrichsen <roerdhh@gmail.com>
2011-01-09 17:08:03 +0100

    Prevent admin users from destroying themselves.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -47,8 +47,13 @@ class UsersController < ApplicationController
   end
 
   def destroy
-    User.find(params[:id]).destroy
-    flash[:success] = "User destroyed."
+    user = User.find(params[:id])
+    if user == current_user
+      flash[:notice] = "You cannot delete yourself."
+    else
+      user.destroy
+      flash[:success] = "User destroyed."
+    end
     redirect_to users_path
   end
   
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -380,8 +380,8 @@ describe UsersController do
     describe "as an admin user" do
 
       before(:each) do
-        admin = Factory(:user, :email => "admin@example.com", :admin => true)
-        test_sign_in(admin)
+        @admin = Factory(:user, :email => "admin@example.com", :admin => true)
+        test_sign_in(@admin)
       end
 
       it "should destroy the user" do
@@ -393,6 +393,14 @@ describe UsersController do
       it "should redirect to the users page" do
         delete :destroy, :id => @user
         response.should redirect_to(users_path)
+      end
+
+      it "prevent admin users from destroying themselves" do
+        lambda do
+          delete :destroy, :id => @admin
+        end.should_not change(User, :count)
+        response.should redirect_to(users_path)
+        flash[:notice].should =~ /delete yourself/i
       end
     end
   end
