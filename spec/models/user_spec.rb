0c451ad74c0b84c3a609910d1e6736a29ce25dc0
Roerd Hinrichsen <roerdhh@gmail.com>
2011-01-20 00:19:12 +0100

    Destroy associated microposts when destroying a user.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -165,5 +165,14 @@ describe User do
     it "should have the right microposts in the right order" do
       @user.microposts.should == [@mp2, @mp1]
     end
+
+    it "should destroy associated microposts" do
+      @user.destroy
+      [@mp1, @mp2].each do |micropost|
+        lambda do
+          Micropost.find(micropost.id)
+        end.should raise_error(ActiveRecord::RecordNotFound)
+      end
+    end
   end
 end
