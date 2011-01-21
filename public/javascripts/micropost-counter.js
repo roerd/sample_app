55afb30a3cf8d6ae74f690c732f666534d83c519
Roerd Hinrichsen <roerdhh@gmail.com>
2011-01-21 15:03:06 +0100

    Beautified the character countdown.

--- public/javascripts/micropost-counter.js
+++ public/javascripts/micropost-counter.js
@@ -2,8 +2,11 @@ document.observe("dom:loaded", function() {
     var content = $('micropost_content');
     var counter = $('micropost_counter');
     function update_counter() {
-        var text = 140 - $F(content).length + " characters left.";
-        counter.update(text);
+        var chars_left = 140 - $F(content).length;
+        counter.update(chars_left.abs() + " character"
+                       + (chars_left.abs() == 1 ? "" : "s")
+                       + (chars_left < 0 ? " too much!" : " left."));
+        counter.setStyle({color: chars_left < 0 ? "red" : "black"});
     }
     update_counter();
     content.observe('keypress', function() {
