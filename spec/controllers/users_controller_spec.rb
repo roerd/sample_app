8c6550ff716d9a14d4bfac606afed23ae5ad23b8
Roerd Hinrichsen <roerdhh@gmail.com>
2011-01-23 09:15:40 +0100

    Added test for the stats on the profile page.

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -150,6 +150,18 @@ describe UsersController do
       response.should have_selector("span.content", :content => content)
       response.should_not have_selector("a", :content => "delete")
     end
+
+    
+    it "should have the right follower/following counts" do
+      test_sign_in(@user)
+      other_user = Factory(:user, :email => Factory.next(:email))
+      other_user.follow!(@user)
+      get :show, :id => @user
+      response.should have_selector("a", :href => following_user_path(@user),
+                                         :content => "0 following")
+      response.should have_selector("a", :href => followers_user_path(@user),
+                                    :content => "1 follower")
+    end
   end
   
   describe "GET 'new'" do
